6235aca91d80351a6a5dacbfd18005dc66eb4e08
Patrick <pdn6991@gmail.com>
2021-06-29 21:33:38 -0400

    Creates update and show for employee model

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -15,4 +15,21 @@ class EmployeesController < ApplicationController
     employee.save
     render json: employee.as_json
   end
+
+  def show
+    employee = Employee.find_by(id: params[:id])
+    render json: employee.as_json
+  end
+
+  def update
+    employee = Employee.find_by(id: params[:id])
+      employee.first_name = params[:first_name] || employee.first_name
+      employee.last_name = params[:last_name] || employee.last_name
+      employee.email = params[:email] || employee.email
+      employee.phone_number = params[:phone_number] || employee.phone_number
+      employee.job_title = params[:job_title] || employee.job_title
+      employee.department_id = params[:department_id] || employee.department_id
+      employee.save
+    render json: employee.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get "/employees", controller: "employees", action: "index"
   post "/employees", controller: "employees", action: "create"
+  get "/employees/:id", controller: "employees", action: "show"
+  patch "/employees/:id" => "employees#update"
+
 end
