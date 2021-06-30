08a3897a9524eedaa95c068b3e866924021a6c59
Patrick <pdn6991@gmail.com>
2021-06-29 21:05:50 -0400

    Created restful routes for employee index and create

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,18 @@
 class EmployeesController < ApplicationController
+  def index
+    render json: Employee.all
+  end
+
+  def create
+    employee = Employee.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number],
+      job_title: params[:job_title],
+      department_id: params[:department_id]
+    )
+    employee.save
+    render json: employee.as_json
+  end
 end
